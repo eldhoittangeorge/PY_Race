ae2aae26cf2b1292d0d137b6fe7cfe0c49abd8ab
eldhoittangeorge <eldhoittangeorge@gmail.com>
2016-11-12 23:18:15 +0530

    made the bountry for the game

--- PY_Race.py
+++ PY_Race.py
@@ -1,7 +1,7 @@
 import pygame
 pygame.init()
-gameWidth = 800
-gameHeight = 600
+gameWidth = 1200
+gameHeight = 720
 black = (0,0,0)
 white = (255,255,255)
 red = (255,0,0)
@@ -13,30 +13,36 @@ clock = pygame.time.Clock()
 
 def car(x,y):
     gameDisplay.blit(carImage,(x,y))
+def gameLoop():
+    x=(gameWidth*.45)
+    y=(gameHeight*.8)
+    x_change = 0
 
-x=(gameWidth*.45)
-y=(gameHeight*.8)
 
-x_change = 0
-crashed = False
-while not crashed:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            crashed = True
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                x_change = -5
-            elif event.key == pygame.K_RIGHT:
-                x_change = 5
+    gameEnd = False
+    while not gameEnd:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                gameEnd = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x_change = -10
+                elif event.key == pygame.K_RIGHT:
+                    x_change = 10
 
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT:
-                x_change = 0
-    x += x_change
-    gameDisplay.fill(green)
-    car(x,y)
 
-    pygame.display.update()
-    clock.tick(60)
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT:
+                    x_change = 0
+
+        x += x_change
+        gameDisplay.fill(green)
+        car(x,y)
+        if x > gameWidth or x < 0 :
+            gameEnd = True
+
+        pygame.display.update()
+        clock.tick(60)
+gameLoop()
 pygame.quit()
 quit()
