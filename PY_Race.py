a79280c7cdcc6d217fb4a4080188cff6cf130254
eldhoittangeorge <eldhoittangeorge@gmail.com>
2016-11-16 22:37:36 +0530

    made a text display

--- PY_Race.py
+++ PY_Race.py
@@ -1,4 +1,6 @@
 import pygame
+import time
+
 pygame.init()
 gameWidth = 1200
 gameHeight = 720
@@ -13,6 +15,19 @@ clock = pygame.time.Clock()
 
 def car(x,y):
     gameDisplay.blit(carImage,(x,y))
+def text_object(text,font):
+    textSurf = font.render(text,True,black)
+    return textSurf,textSurf.get_rect()
+def message_display(text):
+    largeText = pygame.font.Font('freesansbold.ttf',120)
+    textSurf,textRect = text_object(text,largeText)
+    textRect.center = ((gameWidth*.5),(gameHeight*.5))
+    gameDisplay.blit(textSurf, textRect)
+    pygame.display.update()
+    time.sleep(2)
+    gameLoop()
+def crash():
+    message_display('you crashed')
 def gameLoop():
     x=(gameWidth*.45)
     y=(gameHeight*.8)
@@ -23,7 +38,7 @@ def gameLoop():
     while not gameEnd:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                gameEnd = True
+                pygame.quit()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
                     x_change = -10
@@ -39,7 +54,7 @@ def gameLoop():
         gameDisplay.fill(green)
         car(x,y)
         if x > gameWidth or x < 0 :
-            gameEnd = True
+            crash()
 
         pygame.display.update()
         clock.tick(60)
